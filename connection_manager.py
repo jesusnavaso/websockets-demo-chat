c1663311f07f8137cabad39ecee2fdbac95d060a
Jesus Navas Orozco <jesus.navas@soprasteria.com>
2024-09-03 20:31:20 +0200

    Fix bug that prevents sending messages after webhook reconnection

--- connection_manager.py
+++ connection_manager.py
@@ -8,12 +8,12 @@ class ConnectionManager:
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
         self.active_connections.append(websocket)
-        print(f"Client connected: {websocket.client.host}")
+        print(f"Client #{id(websocket)} connected: {websocket.client.host}")
         await self.broadcast(f"Client #{id(websocket)} has joined the chat")
 
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
-        print(f"Client disconnected: {websocket.client.host}")
+        print(f"Client #{id(websocket)} disconnected: {websocket.client.host}")
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
--- main.py
+++ main.py
@@ -33,40 +33,59 @@ async def get():
             <input id="input" autocomplete="off" placeholder="Type your message here..." />
             <button>Send</button>
         </form>
+        <button id="disconnectButton">Disconnect</button>
         <script>
-            const ws = new WebSocket("ws://localhost:8000/ws");
-            const messages = document.getElementById('messages');
-            const form = document.getElementById('form');
-            const input = document.getElementById('input');
+            let ws;
+            const reconnectionDelaySeconds = 10;
+            const connectWebSocket = () => {
+                ws = new WebSocket("ws://localhost:8000/ws");
+                const messages = document.getElementById('messages');
+                const form = document.getElementById('form');
+                const input = document.getElementById('input');
 
-            ws.onmessage = function(event) {
-                const messageItem = document.createElement('li');
-                messageItem.textContent = event.data;
-                messages.appendChild(messageItem);
-                window.scrollTo(0, document.body.scrollHeight);
-            };
+                ws.onmessage = function(event) {
+                    const messageItem = document.createElement('li');
+                    messageItem.textContent = event.data;
+                    messages.appendChild(messageItem);
+                    window.scrollTo(0, document.body.scrollHeight);
+                };
 
-            // Handle the WebSocket onclose event. You can execute we.close() in the browser console
-            ws.onclose = function(event) {
-                console.log("Disconnected from the WebSocket server");
-                const messageItem = document.createElement('li');
-                messageItem.textContent = "You have been disconnected from the chat";
-                messageItem.style.color = "red";
-                messages.appendChild(messageItem);
-            };
+                // Handle the WebSocket onclose event. You can execute we.close() in the browser console
+                ws.onclose = function(event) {
+                    console.log("Disconnected from the WebSocket server");
+                    const messageItem = document.createElement('li');
+                    messageItem.textContent = `You have been disconnected from the chat. Retrying connection in ${reconnectionDelaySeconds}s`;
+                    messageItem.style.color = "red";
+                    messages.appendChild(messageItem);
+                    // Attempt to reconnect after a 10 seconds delay
+                    setTimeout(function() {
+                        connectWebSocket();
+                    }, reconnectionDelaySeconds * 1000);
+                };
 
-            form.addEventListener('submit', function(event) {
-                event.preventDefault();
-                if (input.value) {
-                    ws.send(input.value);
-                    input.value = '';
+                form.addEventListener('submit', function(event) {
+                    event.preventDefault(); // Default form submission reloads the page.
+                    if (input.value) {
+                        ws.send(input.value);
+                        input.value = '';
+                    }
+                });
+
+                // Actions to perform before closing the browser window
+                // window.addEventListener('beforeunload', function() {
+                //     ws.send("The client has closed the tab");
+                // });
+            }
+
+            // Manually close the websocket connection when clicking the Disconnect button
+            disconnectButton.addEventListener('click', function() {
+                if (ws) {
+                    ws.close();
                 }
             });
 
-            // Actions to perform before closing the browser window
-            // window.addEventListener('beforeunload', function() {
-            //     ws.send("The client has closed the tab");
-            // });
+            // Initial connection
+            connectWebSocket();
         </script>
     </body>
     </html>
